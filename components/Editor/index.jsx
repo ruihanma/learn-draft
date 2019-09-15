be63a433657fe6f617ffa17aa8420f68bcb418ed
maruihan <phenix_infinity@hotmail.com>
2019-09-15 19:23:41 +0800

    修复图片输出问题drafttohtml

--- components/Editor/index.jsx
+++ components/Editor/index.jsx
@@ -4,8 +4,14 @@ import {
   EditorState,
   RichUtils,
   AtomicBlockUtils,
-  getDefaultKeyBinding
+  getDefaultKeyBinding,
+  convertToRaw
 } from "draft-js";
+
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
+
+import _ from "lodash";
 
 import Toolbar from "../Toolbar";
 // Style
@@ -52,17 +58,74 @@ function getBlockStyle(block) {
   }
 }
 
+// ADD IMAGE //
+function mediaBlockRenderer(block) {
+  if (block.getType() === "atomic") {
+    return {
+      component: Media,
+      editable: false
+    };
+  }
+
+  return null;
+}
+
+const Image = props => {
+  return <img src={props.src} />;
+};
+
+const Media = props => {
+  const entity = props.contentState.getEntity(props.block.getEntityAt(0));
+  const { src } = entity.getData();
+  const type = entity.getType();
+
+  let media,
+    Type = _.upperCase(type);
+
+  switch (Type) {
+    case "IMAGE":
+      media = <Image src={src} />;
+      break;
+    case "AUDIO":
+      media = <Audio src={src} />;
+      break;
+    case "VIDEO":
+      media = <Video src={src} />;
+      break;
+    default:
+      <span></span>;
+      break;
+  }
+
+  return media;
+};
+
 export default class EditorComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(),
+
+      // ADD IMAGE //
+      showURLInput: false,
+      url: "",
+      urlType: ""
     };
     this.focus = () => this.refs.editor.focus();
     this.handleKeyCommand = this._handleKeyCommand.bind(this);
     this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
     this.toggleBlockType = this._toggleBlockType.bind(this);
     this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
+
+    // ADD IMAGE //
+    this.onURLChange = e => this.setState({ urlValue: e.target.value });
+    this.logState = () => {
+      const content = this.state.editorState.getCurrentContent();
+      console.log(draftToHtml(convertToRaw(content)));
+    };
+    this.addImage = this._addImage.bind(this);
+    this.confirmMedia = this._confirmMedia.bind(this);
+    this.onURLInputKeyDown = this._onURLInputKeyDown.bind(this);
   }
 
   _handleKeyCommand(command, editorState) {
@@ -90,8 +153,43 @@ export default class EditorComponent extends React.Component {
 
   render() {
     const { editorState } = this.state;
+
+    let className = "RichEditor-editor";
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (
+        contentState
+          .getBlockMap()
+          .first()
+          .getType() !== "unstyled"
+      ) {
+        className += " RichEditor-hidePlaceholder";
+      }
+    }
+
+    // ADD IMAGE //
+    let urlInput;
+    if (this.state.showURLInput) {
+      urlInput = (
+        <div>
+          <input
+            onChange={this.onURLChange}
+            ref="url"
+            type="text"
+            value={this.state.urlValue}
+            onKeyDown={this.onURLInputKeyDown}
+          />
+          <button onMouseDown={this.confirmMedia}>Confirm</button>
+        </div>
+      );
+    }
+
     return (
       <Fragment>
+        <button onMouseDown={this.addImage} style={{ marginRight: 10 }}>
+          Add Image
+        </button>
+        {urlInput}
         <Toolbar
           editorState={editorState}
           inlineStyles={INLINE_STYLES}
@@ -101,6 +199,7 @@ export default class EditorComponent extends React.Component {
         />
         <div className="ml-3 mr-3 p-2" style={{ border: `1px solid #333` }}>
           <Editor
+            blockRendererFn={mediaBlockRenderer}
             blockStyleFn={getBlockStyle}
             placeholder="Please Text Here"
             editorState={this.state.editorState}
@@ -112,6 +211,7 @@ export default class EditorComponent extends React.Component {
             customStyleMap={styleMap}
           />
         </div>
+        <input onClick={this.logState} type="button" value="Log State" />
       </Fragment>
     );
   }
@@ -128,6 +228,71 @@ export default class EditorComponent extends React.Component {
   }
 
   onChange = editorState => {
-    this.setState({ editorState });
+    this.setState({ editorState }, () => {
+      console.log(
+        "content: ",
+        draftToHtml(convertToRaw(editorState.getCurrentContent()))
+      );
+      console.log(
+        "convertToRaw",
+        convertToRaw(editorState.getCurrentContent())
+      );
+    });
   };
+
+  //////// ADD IMAGE /////////////
+  _promptForMedia(type) {
+    this.setState(
+      {
+        showURLInput: true,
+        urlValue: "",
+        urlType: type
+      },
+      () => {
+        setTimeout(() => this.refs.url.focus(), 0);
+      }
+    );
+  }
+
+  _addImage() {
+    this._promptForMedia("image");
+  }
+
+  _confirmMedia(e) {
+    e.preventDefault();
+    const { editorState, urlValue, urlType } = this.state;
+    console.log("urlType", urlType);
+    const contentState = editorState.getCurrentContent();
+    const contentStateWithEntity = contentState.createEntity(
+      _.upperCase(urlType),
+      "IMMUTABLE",
+      { src: urlValue, width: 100 }
+    );
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    console.log("entityKey", entityKey);
+    const newEditorState = EditorState.set(editorState, {
+      currentContent: contentStateWithEntity
+    });
+
+    this.setState(
+      {
+        editorState: AtomicBlockUtils.insertAtomicBlock(
+          newEditorState,
+          entityKey,
+          " "
+        ),
+        showURLInput: false,
+        urlValue: ""
+      },
+      () => {
+        setTimeout(() => this.focus(), 0);
+      }
+    );
+  }
+
+  _onURLInputKeyDown(e) {
+    if (e.which === 13) {
+      this._confirmMedia(e);
+    }
+  }
 }
